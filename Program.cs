3834844af71d77b7a1a5e8caac9fe4764120a099
LrBrbt <pogodinalerka@gmail.com>
2023-04-11 14:02:25 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using EQFCS._4;
+using System.Collections.Generic;
+using System.Linq;
+
+
+Console.WriteLine("Задание 1");
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(Tasks.Task1(number));
+
+Console.WriteLine();
+
+Console.WriteLine("Задание 2");
+
+Console.Write("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введите число от 1 до {arraySize}: ");
+int elementNumber = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[arraySize];
+
+Random random = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(-10, 10);
+}
+
+while (array.Length > 1)
+{
+    Console.WriteLine();
+
+    array = Tasks.Task2(array, elementNumber);
+
+    Console.WriteLine();
+    //Console.WriteLine("Удаление элементов.");
+    //Thread.Sleep(1000);
+    //Console.WriteLine("Удаление элементов..");
+    //Thread.Sleep(1000);
+    Console.WriteLine();
+    Thread.Sleep(1000);
+    Console.WriteLine();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+//Console.WriteLine("Задание 3");
---
+++ Tasks.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQFCS._4
+{
+    public static class Tasks
+    {
+        public static string Task1(int number)
+        {
+            int numberPlus1 = number + 1;
+            int numberMinus1 = number - 1;
+
+            return $"{numberMinus1}{number}{numberPlus1}";
+        }
+
+        public static int[] Task2(int[] array, int elementNumber)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            var temp = new List<int>(array);
+
+            for (int i = temp.Count / elementNumber * elementNumber - 1; i >= 0; i -= elementNumber)
+            {
+                temp.RemoveAt(i);
+            }
+            var result = temp.ToArray();
+
+            return result;
+        }
+
+    }
+}
